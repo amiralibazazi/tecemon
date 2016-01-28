6e25ab11cffc216a1481102910caca7db759508b
Amir Bazazi <amiralibazazi@gmail.com>
2016-01-28 11:51:53 +0000

    Add directive to constrict mainview when sidebar appears

--- public/js/controllers/sidebarController.js
+++ public/js/controllers/sidebarController.js
@@ -3,28 +3,39 @@
 var module = angular.module('sidebarcontroller', []);
 
 module.controller('sidebarController', function($scope) {
-    
-    $scope.state = true;
-    
+    $scope.state = false;
+
     $scope.toggleNavBar = function() {
         $scope.state = !$scope.state;
     };
-    
 });
 
 module.directive('sidebarDirective', function() {
     return {
         link : function(scope, element, attr) {
             scope.$watch(attr.sidebarDirective, function(newVal) {
-                  if(newVal)
-                  {
-                    element.addClass('show'); 
-                    return;
-                  }
-                  element.removeClass('show');
+                if(newVal) {
+                  element.addClass('show'); 
+                  return;
+                }
+                element.removeClass('show');
             });
         }
     };
+});
+
+module.directive('constrictDirective', function() {
+    return {
+        link : function(scope, element, attr) {
+            scope.$watch(attr.constrictDirective, function(newVal) {
+                if (newVal) {
+                  element.addClass('constrict');
+                  return;
+                }
+                element.removeClass('constrict');
+            })
+        }
+    }
 });
     
 }());
